31da2f11a5008c64458644ae9dd79a0701425aa7
Harrison Held <harrydheld@gmail.com>
2026-04-05 19:19:03 -0500

    simplified

--- instrumentor.py
+++ instrumentor.py
@@ -2,60 +2,36 @@ import ast
 
 class ClauseInstrumentor(ast.NodeTransformer):
     def __init__(self):
-        self.counter = 0
+        self.clause_count = 0
         self.clause_ids = []
         self.clause_text_by_id = {}
 
-    def new_id(self):
-        self.counter += 1
-        return f"clause_{self.counter}"
-
-    def wrap_clause(self, node):
-        """Wrap a node so that record(...) is called but original value returned"""
-        clause_id = self.new_id()
-        self.clause_ids.append(clause_id)
-        try:
-            clause_text = ast.unparse(node)
-        except Exception:
-            clause_text = "<unknown>"
-        self.clause_text_by_id[clause_id] = clause_text
-        return ast.Call(
-            func=ast.Lambda(
-                args=ast.arguments(posonlyargs=[], args=[ast.arg(arg="v")], kwonlyargs=[], kw_defaults=[], defaults=[]),
-                body=ast.Call(
-                    func=ast.Name(id="record", ctx=ast.Load()),
-                    args=[ast.Constant(value=clause_id), ast.Name(id="v", ctx=ast.Load())],
-                    keywords=[]
-                )
-            ),
-            args=[node],
-            keywords=[]
-        )
-
     #
     # When ClauseInstrumentor.visit() is called, these specific methods will get called when the type of node is visited
-    # Example: visit_Compare is called when a Compare node is visited
+    # Example: visit_If is called when a If node is visited
     # All of these are documented here: https://docs.python.org/3/library/ast.html
-    # In each method I paraphrased some notes from the above link
     #
-    def visit_Compare(self, node):
-        # Example "a > 3"
+    def visit_If(self, node: ast.If):
+        node.test = self.visit_predicate(node.test)
         self.generic_visit(node)
-        return self.wrap_clause(node)
-
-    def visit_BoolOp(self, node):
-        # A bool op is 'or' or 'and'.
-        # values are the expressions involved. so for "a or b", "a" and "b" would be the values.
-        # There may be more than 2, such as "a and b and c".
-        self.generic_visit(node)
-        node.values = [self.wrap_clause(value) for value in node.values]
         return node
 
-    def visit_UnaryOp(self, node):
-        # A UnaryOp is something that takes one argument, such as "not x".
-        # "not" would be the op
-        # "x" would be the operand
+    def visit_While(self, node: ast.While):
+        node.test = self.visit_predicate(node.test)
         self.generic_visit(node)
-        if isinstance(node.op, ast.Not):
-            return self.wrap_clause(node)
         return node
+    
+    # visit_predicate will visit the thing that is inside if(...) and while(...) statements
+    # it takes in the expression (...), and returns an expression with instrumentation added
+    def visit_predicate(self, expression: ast.expr) -> ast.expr:
+        self.clause_count += 1
+        clause_id = "clause" + str(self.clause_count)
+        self.clause_ids.append(clause_id)
+        # ast.unparse returns source code
+        self.clause_text_by_id[clause_id] = ast.unparse(expression)
+
+        return ast.Call(
+            func=ast.Name(id="record", ctx=ast.Load()),
+            args=[ast.Constant(value=clause_id), expression],
+            keywords=[]
+        )
--- target1.py
+++ target1.py
@@ -2,6 +2,12 @@ def the_function(a: bool, b: bool):
     if (a and b):
         pass
 
+    while(False):
+        pass
+
+    if(3 > 2):
+        pass
+
 the_function(False, False)
 the_function(True, False)
 the_function(False, True)
